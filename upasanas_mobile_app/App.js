6c4c5b7c026a8264c6263b3b75d0ac322563d2ec
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-09-23 22:18:51 -0700

    react assignment files added

---
+++ upasanas_mobile_app/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './styles/MyStyles';
+
+function App() {
+  return (
+      <View style={styles.foo}>
+        <Text> Upasana Mukherjee's App</Text>
+      </View>
+    );
+}
+
+export default App;
---
+++ upasanas_mobile_app/styles/MyStyles.js
@@ -0,0 +1,12 @@
+import {Stylesheet} from 'react-native';
+
+var styles = {
+  foo: {
+    flex:1,
+    justifyContent: "center",
+    alignItem: "center",
+    backgroundColor: "#ADD"
+  }
+};
+
+export default styles;
