84b73a2e1a332a64440c2a69d488afbd54fc7b68
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-09-29 21:32:12 -0700

    two new components added - image background and progress bar android

--- upasanas_mobile_app/App.js
+++ upasanas_mobile_app/App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {DatePickerIOS,StyleSheet, ProgressBarAndroid} from 'react-native';
 import styles from './styles/MyStyles';
 import Status from './comp/Status';
 import Avatar from './comp/Avatar';
@@ -18,8 +19,25 @@ function App() {
         <Text style={styles.apptext}> Upasana Mukherjee's App</Text>
         <Status />
         <Avatar />
+        <ProgressBarAndroid styleAttr="Horizontal" />
+        <ProgressBarAndroid styleAttr="Horizontal" color="#2196F3" />
+        <ProgressBarAndroid
+          styleAttr="Horizontal"
+          indeterminate={false}
+          progress={0.5}
+        />
       </View>
     );
 }
 
+function ImageBg(){
+  return (
+  <ImageBackground source={'https://www.pinclipart.com/picdir/middle/367-3676898_report-abuse-cute-ice-cream-drawing-clipart.png'}
+  style={{width: '100%', height: '100%'}}>
+    <Text>The first component is image background</Text>
+  </ImageBackground>
+);
+}
+
 export default App;
+export default ImageBg;
